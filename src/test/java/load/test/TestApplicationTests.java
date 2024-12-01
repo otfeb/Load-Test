f3e54b2095287025ca9a4b4ac8056c563368b163
quro <hs970216@naver.com>
2024-12-01 20:48:13 +0900

    feat: initial setting

---
+++ src/test/java/load/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package load.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
